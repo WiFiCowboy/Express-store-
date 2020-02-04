bcb9a33bc7273f10dca62396196124e52aeffe2e
Humberto Hernandez <hamburger45@gmail.com>
2020-02-03 20:00:57 -0800

    edit readMe

--- src/app.js
+++ src/app.js
@@ -1,24 +1,10 @@
-// require('dotenv').config();
 const express = require('express');
 const morgan = require('morgan');
-// const cors = require('cors');
-// const helmet = require('helmet');
-// const { NODE_ENV } = require('./config');
 
 const app = express();
 
-// const morganOption = NODE_ENV === 'production' ? 'tiny' : 'common';
-
-// app.use(morgan(morganOption));
 app.use(morgan('dev'));
 app.use(express.json());
-// app.use(helmet());
-// app.use(cors());
-
-// app.post('/', (req, res) => {
-// 	console.log(req.body);
-// 	res.send('POST request received.');
-// });
 
 app.post('/user', (req, res) => {
 	// get the data
@@ -68,20 +54,5 @@ app.post('/user', (req, res) => {
 	// at this point all validation passed
 	res.send('All validation passed');
 });
-
-// app.get('/', (req, res) => {
-// 	res.send('A GET request');
-// });
-
-// app.use(function errorHandler(error, req, res, next) {
-// 	let response;
-// 	if (NODE_ENV === 'production') {
-// 		response = { error: { message: 'server error' } };
-// 	} else {
-// 		console.error(error);
-// 		response = { message: error.message, error };
-// 	}
-// 	res.status(500).json(response);
-// });
 
 module.exports = app;
